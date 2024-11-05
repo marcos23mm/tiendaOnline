5fd88a4c2ab3af3f07d78a3cee48d9aa3fe80cef
marcos23mm <161719402+marcos23mm@users.noreply.github.com>
2024-11-05 11:39:40 +0100

    Create db.php

---
+++ Modelo/db.php
@@ -0,0 +1,21 @@
+<?php
+class DB {
+    private static $host = 'localhost';
+    private static $dbName = 'tiendaPHP';
+    private static $username = 'root';
+    private static $password = '123';
+    private static $conn = null;
+
+    public static function getConnection() {
+        if (self::$conn === null) {
+            try {
+                self::$conn = new PDO("mysql:host=" . self::$host . ";dbname=" . self::$dbName, self::$username, self::$password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Error de conexión: " . $e->getMessage();
+            }
+        }
+        return self::$conn;
+    }
+}
+?>
