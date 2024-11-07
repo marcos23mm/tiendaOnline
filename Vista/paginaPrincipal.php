7422b06dbac948e5ad6f9ef66eed2c99b131db8e
TampaxCompak <dpf109@educa.madrid.org>
2024-11-07 12:18:14 +0100

    Add files via upload

---
+++ Vista/paginaPrincipal.php
@@ -0,0 +1,58 @@
+<?php
+session_name("sesion1");
+session_start();
+
+$servername = "localhost";
+$dbname = "TiendaPHP";
+$username = "root";
+$password = "";
+
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stmt = $conn->prepare("SELECT * FROM producto");
+$stmt->execute();
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Electrónica Calico</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Descripción</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+    </tr>
+    <?php
+    $listaResultados = $stmt->fetchAll();
+
+    foreach ($listaResultados as $row) {
+        print "<tr>";
+        print "<td>" . $row["id"] . "</td>";
+        print "<td>" . $row["nombre"] . "</td>";
+        print "<td>" . $row["descripcion"] . "</td>";
+        print "<td>" . number_format($row["precio"], 2) . " €</td>";
+        print '<td><button id="comprar" onclick="agregarCarrito(' . $row["id"] . ')">Añadir al carrito</button></td>';
+        print "</tr>";
+    }
+    ?>
+</table>
+<?php
+if (isset($_SESSION['nickname'])) {
+    $nickname = $_SESSION['nickname'];
+    print "Está conectado con este usuario: " . $nickname;
+} else {
+    print "No hay usuario registrado.";
+}
+?>
+<p><a href="formAdd.php">Añadir producto</a></p>
+<p><a href="formDelete.php">Borrar producto</a></p>
+<p><a href="formUpdate.php">Actualizar producto</a></p>
+<p><a href="formSelect.php">Seleccionar producto</a></p>
+</body>
+</html>
