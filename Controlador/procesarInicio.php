9769d7f8d6c88549f66f7a93f68befd40ebb7253
TampaxCompak <dpf109@educa.madrid.org>
2024-11-15 08:25:14 +0100

    Update procesarInicio.php

--- Controlador/procesarInicio.php
+++ Controlador/procesarInicio.php
@@ -1,16 +1,4 @@
 <?php
-
-session_name("");
-session_start();
-require_once '../Modelo/db.php';
-
-if (isset($_POST['cliente']) && isset($_POST['contraseña'])) {
-    $nickname = $_POST['cliente'];
-    $password1 = $_POST['contraseña'];
-}
-
-try {
-    // Obtener la conexión usando la clase DB<?php
 session_start();
 require_once '../Modelo/DAOCliente.php';
 
@@ -37,23 +25,3 @@ if (isset($_POST['cliente']) && isset($_POST['contraseña'])) {
     }
 }
 ?>
-    $conn = DB::getConnection();
-
-    $stmt = $conn->prepare("SELECT * FROM Cliente WHERE nickname = :nombreUser AND password = :contrasena");
-    $stmt->bindParam(':nombreUser', $nickname);
-    $stmt->bindParam(':contrasena', $password1);
-
-    $stmt->execute();
-
-    $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($usuario) {
-        $_SESSION['usuario'] = $nickname;
-        header('Location: ../Vista/paginaPrincipal.php');
-    } else {
-        echo "Nombre de usuario o contraseña incorrectos";
-    }
-
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
